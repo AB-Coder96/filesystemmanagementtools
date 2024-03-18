445c808743ed2754f403c14ae9fb79c2f215e64d
Araz Bagherzadeh Karimi <arazbaghezadeh@gmail.com>
2024-03-18 04:25:35 -0400

    reg updates (ArazKarimi-Laptop)

--- InputCLIS/inputfunctions.py
+++ InputCLIS/inputfunctions.py
@@ -32,17 +32,29 @@ def get_date_time_input():
             except ValueError:
                 print("Invalid input. Please enter valid numerical values.")
 def select_type(List):
-        for idx, List_type in enumerate(List, start=1):
-            print(f"{idx}. {List_type}")
-        
-        while True:
-            selection = input(f"Enter the number corresponding to the List type: ")
-            try:
-                selection = int(selection)
-                if 1 <= selection <= len(List):
-                    selected_type = List[selection - 1]
-                    return selected_type
-                else:
-                    print("Please enter a valid number from the list.")
-            except ValueError:
-                print("Please enter a valid number.")
+    # Calculate the midpoint of the list
+    midpoint = len(List) // 2
+    
+    # Print the first half of the list in the first row
+    for idx, List_type in enumerate(List[:midpoint], start=1):
+        print(f"{idx}. {List_type}", end='\t')
+    
+    print()  # Print a newline to move to the next row
+    
+    # Print the second half of the list in the second row
+    for idx, List_type in enumerate(List[midpoint:], start=midpoint + 1):
+        print(f"{idx}. {List_type}", end='\t')
+    
+    print()  # Print a newline to move to the next row
+    
+    while True:
+        selection = input(f"Enter the number corresponding to the List type: ")
+        try:
+            selection = int(selection)
+            if 1 <= selection <= len(List):
+                selected_type = List[selection - 1]
+                return selected_type
+            else:
+                print("Please enter a valid number from the list.")
+        except ValueError:
+            print("Please enter a valid number.")
